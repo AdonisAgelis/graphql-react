5ef12500149e1d04ea11c83e364ea417fd469bc1
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2021-12-04 03:33:50 +0200

    SongDetail component

---
+++ client/components/SongDetail.js
@@ -0,0 +1,14 @@
+import { divide } from 'lodash';
+import React, { Component } from 'react';
+
+class SongDetail extends Component {
+  render() {
+    return (
+      <div>
+        <h3></h3>
+      </div>
+    );
+  }
+}
+
+export default SongDetail;
